51958ba050b7e82172a205247a9381c51156d82d
Sobit Prasad <sobitp59@gmail.com>
2022-06-22 23:36:53 +0530

    taking input from the user

---
+++ Python/DAY  2/dayTwo.py
@@ -0,0 +1,13 @@
+# 🚀 Taking input from the user
+"""
+name = input('What is your name? ')
+print('Welcome ', name)
+age = int(input('What is your age? '))
+print('Your age : ', age, 'Type of age is : ', type(age))
+# inputs take values in string by default, so we may need to typecast
+"""
+
+
+
+
+

33c6ac9d96d231a6e9aa419750d43a69289de576
Sobit Prasad <sobitp59@gmail.com>
2022-06-23 00:07:12 +0530

    mutability and immutability

--- Python/DAY  2/dayTwo.py
+++ Python/DAY  2/dayTwo.py
@@ -7,6 +7,36 @@ print('Your age : ', age, 'Type of age is : ', type(age))
 # inputs take values in string by default, so we may need to typecast
 """
 
+# 🚀 Mutability and immutability
+
+"""
+I referred this blog to dive deep in this topic : https://www.mygreatlearning.com/blog/understanding-mutable-and-immutable-in-python/#M&I
+
+:: LISTS ARE MUTABLE
+programmingLanguages = ['Python', 'JavaScript', 'Java ']
+for programmingLanguage in programmingLanguages:
+    print(programmingLanguage, end=' ')
+print(hex(id(programmingLanguages)))
+
+programmingLanguages.append('C++')
+for programmingLanguage in programmingLanguages:
+    print(programmingLanguage, end=' ')
+print(hex(id(programmingLanguages)))
+
+# ids are same before and after which means it doesn't form a new list and hence it is mutable
+
+
+:: STRINGS ARE IMMUTABLE
+
+name = 'Sobit'
+print(name, hex(id(name)))
+
+x = name.replace('Sobit', 'Rohit')
+print(x, hex(id(x)))
+"""
+
+
+
 
 
 

bf0c4ded2d76a9caa0cca727bfb93ee1af0ba706
Sobit Prasad <sobitp59@gmail.com>
2022-06-26 22:36:25 +0530

    character frequency using dictionary

--- THA/DAY 2/characters_frequency.py
+++ THA/DAY 2/characters_frequency.py
@@ -1,3 +1,16 @@
-st = 'hello sobitttt prasadadda'
-m = (st[4])
-print(m)
+my_str = 'sobit learning python from Kshitiz Sir'
+
+str_freq = {}
+str_freq_arr = []
+
+for s in my_str:
+    if s not in str_freq:
+        str_freq[s] = 1
+    else:
+        str_freq[s] = str_freq[s] + 1
+
+    for key in str_freq:
+        val = str_freq[key]
+    str_freq_arr.append(val)
+print(str_freq)
+
